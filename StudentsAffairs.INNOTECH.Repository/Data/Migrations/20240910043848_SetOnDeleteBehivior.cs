b1a99d5b1b51b03d75b2f40df263c29f1be2788a
roma2 <roma2001342@gmail.com>
2024-09-11 04:47:36 +0300

    Add project files.

---
+++ StudentsAffairs.INNOTECH.Repository/Data/Migrations/20240910043848_SetOnDeleteBehivior.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentsAffairs.INNOTECH.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetOnDeleteBehivior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Departments_DepartmentId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Professors_Departments_DepartmentId",
+                table: "Professors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Students",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Professors",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Departments_DepartmentId",
+                table: "Courses",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Professors_Departments_DepartmentId",
+                table: "Professors",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Departments_DepartmentId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Professors_Departments_DepartmentId",
+                table: "Professors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Professors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Departments_DepartmentId",
+                table: "Courses",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Professors_Departments_DepartmentId",
+                table: "Professors",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Departments_DepartmentId",
+                table: "Students",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ StudentsAffairs.INNOTECH.Repository/Data/Migrations/20240910050225_OnDelete.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentsAffairs.INNOTECH.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Courses_CourseId",
+                table: "Enrollments",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+    }
+}
